a0e1dcf53de53fb884c36dbca52d19a8afb86b84
Óscar Corbalán <oscar.corbalanfernandez@plexus.es>
2025-01-31 09:49:17 +0100

    feat: create socket server

---
+++ models/server.js
@@ -0,0 +1,43 @@
+// Servidor de express
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+const path = require('path');
+const Sockets = require('./sockets');
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+
+    // HTTP server
+    this.server = http.createServer(this.app);
+
+    // Configuraciones de sockets
+    this.io = socketio(this.server);
+  }
+
+  middleWares() {
+    // Desplegar el directorio público
+    this.app.use(express.static(path.resolve(__dirname, '../public')));
+  }
+
+  configurarSockets() {
+    new Sockets(this.io);
+  }
+
+  execute() {
+    // inicializar Middlewares
+    this.middleWares();
+
+    // Inicializar sockets
+    this.configurarSockets();
+
+    // inicializar servidor
+    this.server.listen(this.port, () => {
+      console.log('Server running on port:', this.port);
+    });
+  }
+}
+
+module.exports = Server;
